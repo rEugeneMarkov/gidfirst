d9b36dad5ed1d0fbcfc956c3f6b40b090c3702ec
rEugeneMarkov <117519379+rEugeneMarkov@users.noreply.github.com>
2022-11-08 00:30:16 +0200

    go_slip

--- check_registration.php
+++ check_registration.php
@@ -12,13 +12,20 @@ session_start();
 
 if (strlen($name) <= 1) {
     $_SESSION['error_username'] = "Введите корректное имя";
+    $_SESSION['success'] = "";
+    $_SESSION['error_email'] = "";
     redirectreg();
 } elseif (strlen($email) < 5) {
         $_SESSION['error_email'] = "Введите корректную почту";
+        $_SESSION['success'] = "";
+        $_SESSION['error_username'] = "";
         redirectreg();
 } else {
-        $mysql->query("INSERT INTO `users` (`id`, `name`, `pass`, `email`, `date`) VALUES (NULL, '$name', '$pass', '$email', CURRENT_TIMESTAMP)");
+        $mysql->query("INSERT INTO `users` (`id`, `name`, `pass`, `email`, `date`) 
+        VALUES (NULL, '$name', MD5('$pass'), '$email', CURRENT_TIMESTAMP)");
 
         $_SESSION['success'] = "Вы успешно зарегистрировались!";
+        $_SESSION['error_username'] = "";
+        $_SESSION['error_email'] = "";
         redirectreg();
 }
