136d317308b0972aaebfcb833ea503f8bdcaff24
Alexander Pirogov <axp.pirogov@gmail.com>
2022-11-09 20:37:13 +0200

    simplified registration

--- check_registration.php
+++ check_registration.php
@@ -18,42 +18,28 @@ session_start();
     $_SESSION['name'] = $name;
     $_SESSION['email'] = $email;
 
+$_SESSION['error_username'] = "";
+$_SESSION['name'] = "";
+$_SESSION['success'] = "";
+$_SESSION['error_email'] = "";
+$_SESSION['error_pass'] = "";
+
 if (strlen($name) <= 1) { // проверка имени
     $_SESSION['error_username'] = "Введите корректное имя";
-    $_SESSION['name'] = "";
-    $_SESSION['success'] = "";
-    $_SESSION['error_email'] = "";
-    $_SESSION['error_pass'] = "";
     redirectreg();
 } elseif (strlen($email) < 7) { //проверка почты
         $_SESSION['error_email'] = "Введите корректную почту";
-        $_SESSION['email'] = "";
-        $_SESSION['success'] = "";
-        $_SESSION['error_username'] = "";
-        $_SESSION['error_pass'] = "";
         redirectreg();
 } elseif ($check_email == "error") { // проверка почты на наличие в базе
         $_SESSION['error_email'] = "Такой пользователь уже зарегистрирован";
-        $_SESSION['email'] = "";
-        $_SESSION['success'] = "";
-        $_SESSION['error_username'] = "";
-        $_SESSION['error_pass'] = "";
         redirectreg();
 } elseif (strlen($pass) < 6) {
         $_SESSION['error_pass'] = "Минимальная длинна пароля 6 символов";
-        $_SESSION['success'] = "";
-        $_SESSION['error_username'] = "";
-        $_SESSION['error_email'] = "";
         redirectreg();
 } else {
         $mysql->query("INSERT INTO `users` (`id`, `name`, `pass`, `email`, `date`) 
         VALUES (NULL, '$name', MD5('$pass'), '$email', CURRENT_TIMESTAMP)");
 
         $_SESSION['success'] = "Вы успешно зарегистрировались!";
-        $_SESSION['error_username'] = "";
-        $_SESSION['error_email'] = "";
-        $_SESSION['error_pass'] = "";
-        $_SESSION['name'] = "";
-        $_SESSION['email'] = "";
         redirectreg();
 }
