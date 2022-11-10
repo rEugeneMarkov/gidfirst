ccc08c748bfc6bcdd71700c0feba93e2b6a95922
rEugeneMarkov <117519379+rEugeneMarkov@users.noreply.github.com>
2022-11-11 01:00:48 +0200

    spatj

--- header.php
+++ header.php
@@ -9,8 +9,33 @@
     <body>
     <div id="wrapper">
 <header>
+<div style="text-align: center;">
+
+        <form method="post">
+        <button name="clear_session" type="submit"class="btn btn-info btn-group">Выйти</button>
+        </form> 
+</div>
+<?php
+session_start();
+if (isset($_POST['clear_session'])) {
+    $_SESSION['is_logined'] = "";
+        $new_url = 'index.php';
+        //redirect($new_url);
+}
+
+    $_SESSION['is_logined'] = $_SESSION['is_logined'];
+    $log = $_SESSION['is_logined'];
+if (strlen($log) > 0) {
+    ?>
+    <div> <?=$_SESSION['is_logined']?></div>
+    <?php
+} else {
+    require"login.php";
+}
+?>
     <a href="index.php">Главная</a> |
-    <a href="registration.php">Регистрация</a> |
     <a href="articles.php">Статьи</a> |
+    <a href="registration.php">Регистрация</a> |
 </header> 
-        <?php
+    
+    
