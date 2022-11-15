6c92384fbd2073c08d4b2c6b48d0ce458c031013
rEugeneMarkov <117519379+rEugeneMarkov@users.noreply.github.com>
2022-11-16 00:39:45 +0200

    template

---
+++ temp_comment.php
@@ -0,0 +1,10 @@
+
+<div class="note">
+    <p>
+        <span class="date"><?= $row['date']?></span>
+        <span class="name"><?= $row['name']?></span>
+    </p>
+    <p>
+        <?= $row['comment']?>
+    </p>
+</div>
